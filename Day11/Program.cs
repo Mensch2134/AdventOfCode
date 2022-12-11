28beca8e27c8f6c5eb819d8274ba6f1c451fe01f
Max <85754826+Mensch2134@users.noreply.github.com>
2022-12-11 14:22:13 +0100

    Day11

---
+++ Day11/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day11;
+
+var monkeys = File.ReadLines("input.txt").Chunk(7).ToList();
+
+List<Monkey> monkeyList = new List<Monkey>();
+int modulator = 1;
+
+foreach(var monkey in monkeys)
+{
+    string items = "";
+    int worryModifier = -1;
+    OperationType modifyType = OperationType.ADD;
+    int throwDivisor = 1;
+    int trueMonkeyIndex = 0;
+    int falseMonkeyIndex = 0;  
+    foreach (var line in monkey)
+    {
+        if (line.Equals("") || line.StartsWith("Monkey"))
+            continue;
+
+        var l = line.Split(':');
+        var indicator = l[0].Trim();
+        var inf = l[1].Split(' ');
+        if (indicator.Equals("Starting items"))
+        {
+            items = l[1].Trim();
+            continue;
+        }
+        if (indicator.Equals("Operation"))
+        {
+            var success = int.TryParse(inf[inf.Length - 1], out worryModifier);
+            if (!success)
+                worryModifier = -1;
+            var op = inf[inf.Length - 2];
+            modifyType = op.Equals("*") ? OperationType.MULTIPLY : OperationType.ADD;
+
+        }
+        if (indicator.Equals("Test"))
+        {
+            throwDivisor = int.Parse(inf[inf.Length - 1]);
+        }
+        if (indicator.Equals("If true"))
+        {
+            trueMonkeyIndex = int.Parse(inf[inf.Length - 1]);
+        }
+        if (indicator.Equals("If false"))
+        {
+            falseMonkeyIndex = int.Parse(inf[inf.Length - 1]);
+        }
+    }
+    modulator *= throwDivisor;
+    monkeyList.Add(new Monkey(items, worryModifier, modifyType, throwDivisor, trueMonkeyIndex, falseMonkeyIndex));
+}
+
+for(int i = 0; i < 10000; i++)
+{
+    foreach(Monkey monkey in monkeyList)
+    {
+        var items = monkey.getItemCount();
+        for (int j = 0; j < items; j++)
+        {
+            //ThrowResult r = monkey.throwItem();
+            ThrowResult r = monkey.throwItemSecond();
+            monkeyList[r.recipient].addItem(r.worry % modulator);
+        }
+    }
+    //Console.WriteLine(i);
+}
+
+monkeyList = monkeyList.OrderBy(x => x.getInspectedItems()).ToList();
+
+int count = 0;
+foreach(Monkey monkey in monkeyList)
+{
+    Console.Write("Monkey " + count + ": ");
+    monkey.print();
+    count++;
+}
+
+long monkeyBusiness = (long)monkeyList[monkeyList.Count - 1].getInspectedItems() * (long)monkeyList[monkeyList.Count - 2].getInspectedItems();
+Console.WriteLine("Zack die Bohne, die Affen sind " + monkeyBusiness + " am business moken.");
---
+++ Day11/monkey.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Day11
+{
+    internal class Monkey
+    {
+        Queue<long> itemWorries;
+        int worryModifier;
+        OperationType modifyType;
+        int throwDivisor;
+        int trueMonkeyIndex;
+        int falseMonkeyIndex;
+        bool squareWorry = false;
+
+        int inspectedItems = 0;
+
+        public Monkey(string items, int worryModifier, OperationType modifyType, int throwDivisor, int trueMonkeyIndex, int falseMonkeyIndex)
+        {
+            this.itemWorries = new Queue<long>();
+            var its = items.Split(", ");
+            foreach (var num in its)
+                itemWorries.Enqueue(int.Parse(num));
+            if (worryModifier == -1)
+                squareWorry = true;
+            this.worryModifier = worryModifier;
+            this.modifyType = modifyType;
+            this.throwDivisor = throwDivisor;
+            this.trueMonkeyIndex = trueMonkeyIndex;
+            this.falseMonkeyIndex = falseMonkeyIndex;
+        }
+
+        public ThrowResult throwItem()
+        {
+            inspectedItems++;
+            var item = itemWorries.Dequeue();
+            item = squareWorry ? (item * item) / 3 : modifyType == OperationType.MULTIPLY ? (item * worryModifier) / 3 : (item + worryModifier) / 3;
+            if (item % throwDivisor == 0)
+                return new ThrowResult(item, trueMonkeyIndex);
+            return new ThrowResult(item, falseMonkeyIndex);
+        }
+
+        public ThrowResult throwItemSecond()
+        {
+            inspectedItems++;
+            var item = itemWorries.Dequeue();
+            item = squareWorry ? (item * item) : modifyType == OperationType.MULTIPLY ? (item * worryModifier) : (item + worryModifier);
+            if (item % throwDivisor == 0)
+                return new ThrowResult(item, trueMonkeyIndex);
+            return new ThrowResult(item, falseMonkeyIndex);
+        }
+
+        public void addItem(long i)
+        {
+            itemWorries.Enqueue(i);
+        }
+
+        public void print()
+        {
+            Queue<long> items = new Queue<long>(itemWorries);
+            var count = items.Count;
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(items.Dequeue() + ", ");
+            }
+            Console.WriteLine(" | interacted with " + inspectedItems + " items.");
+        }
+
+        public int getItemCount() { return itemWorries.Count; }
+
+        public int getInspectedItems() { return inspectedItems; }
+    }
+
+    internal struct ThrowResult
+    {
+        public long worry;
+        public int recipient;
+
+        public ThrowResult(long w, int r)
+        {
+            worry = w; recipient = r;
+        }
+    }
+
+    public enum OperationType{
+        MULTIPLY, 
+        ADD
+    }
+}
